75c2eec8981b89e9ce115ca3dd8800fd640aa096
Jacob Jude <jacobj.jude@outlook.com>
2022-06-29 15:55:37 -0400

    partial database added

---
+++ scripts/database.js
@@ -0,0 +1,70 @@
+const database = {
+	services: [
+		{
+			id: 1,
+			name: 'Rafting'
+		},
+		{
+			id: 2,
+			name: 'Canoeing'
+		},
+		{
+			id: 3,
+			name: 'Fishing'
+		},
+		{
+			id: 4,
+			name: 'Hiking'
+		},
+		{
+			id: 5,
+			name: 'Picnicking'
+		},
+		{
+			id: 6,
+			name: 'Rock Climbing'
+		},
+		{
+			id: 7,
+			name: 'Lodging'
+		},
+		{
+			id: 8,
+			name: 'Parking'
+		},
+		{
+			id: 9,
+			name: 'Information'
+		},
+		{
+			id: 10,
+			name: 'Ziplines'
+		}
+	],
+	parkAreas: [
+		{
+			id: 1,
+			name: 'Lodge'
+		},
+        {
+            id: 2,
+            name: 'Lost Wolf Hiking Trail'
+        },
+        {
+            id: 3,
+            name: 'Chamfort River'
+        },
+        {
+            id: 4,
+            name: 'Gander River'
+        },
+        {
+            id: 5,
+            name: 'Campground'
+        },
+        {
+            id: 6,
+            name: 'Pine Bluff Trails'
+        }
+	]
+};
