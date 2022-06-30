5f5d3dc4165d8b6ca756861f646a9242ba6961a4
Jacob Jude <jacobj.jude@outlook.com>
2022-06-30 10:44:41 -0400

    added random guest generator

--- scripts/database.js
+++ scripts/database.js
@@ -1,4 +1,7 @@
-const database = {
+import { generateGuests } from './random.js';
+
+const guestList = generateGuests();
+export const database = {
 	services: [
 		{
 			id: 1,
@@ -46,25 +49,118 @@ const database = {
 			id: 1,
 			name: 'Lodge'
 		},
-        {
-            id: 2,
-            name: 'Lost Wolf Hiking Trail'
-        },
-        {
-            id: 3,
-            name: 'Chamfort River'
-        },
-        {
-            id: 4,
-            name: 'Gander River'
-        },
-        {
-            id: 5,
-            name: 'Campground'
-        },
-        {
-            id: 6,
-            name: 'Pine Bluff Trails'
-        }
-	]
+		{
+			id: 2,
+			name: 'Lost Wolf Hiking Trail'
+		},
+		{
+			id: 3,
+			name: 'Chamfort River'
+		},
+		{
+			id: 4,
+			name: 'Gander River'
+		},
+		{
+			id: 5,
+			name: 'Campground'
+		},
+		{
+			id: 6,
+			name: 'Pine Bluff Trails'
+		}
+	],
+	offer: [
+		{
+			id: 1,
+			parkId: 1,
+			serviceId: 7
+		},
+		{
+			id: 2,
+			parkId: 1,
+			serviceId: 8
+		},
+		{
+			id: 3,
+			parkId: 1,
+			serviceId: 9
+		},
+		{
+			id: 4,
+			parkId: 1,
+			serviceId: 5
+		},
+		{
+			id: 5,
+			parkId: 2,
+			serviceId: 4
+		},
+		{
+			id: 6,
+			parkId: 2,
+			serviceId: 5
+		},
+		{
+			id: 7,
+			parkId: 2,
+			serviceId: 6
+		},
+		{
+			id: 8,
+			parkId: 3,
+			serviceId: 1
+		},
+		{
+			id: 9,
+			parkId: 3,
+			serviceId: 2
+		},
+		{
+			id: 10,
+			parkId: 3,
+			serviceId: 3
+		},
+		{
+			id: 11,
+			parkId: 4,
+			serviceId: 3
+		},
+		{
+			id: 12,
+			parkId: 4,
+			serviceId: 4
+		},
+		{
+			id: 13,
+			parkId: 5,
+			serviceId: 9
+		},
+		{
+			id: 14,
+			parkId: 5,
+			serviceId: 7
+		},
+		{
+			id: 15,
+			parkId: 5,
+			serviceId: 8
+		},
+		{
+			id: 16,
+			parkId: 6,
+			serviceId: 4
+		},
+		{
+			id: 17,
+			parkId: 6,
+			serviceId: 5
+		},
+		{
+			id: 18,
+			parkId: 6,
+			serviceId: 10
+		}
+	],
+	parkGuests: guestList
 };
---
+++ scripts/random.js
@@ -0,0 +1,146 @@
+export const firstNames = [
+	'Edmond',
+	'Janelle',
+	'Sonia',
+	'Noelia',
+	'Jayne',
+	'Ellen',
+	'Caroline',
+	'Charity',
+	'Conrad',
+	'Maude',
+	'Leonie',
+	'Sean',
+	'Judah',
+	'Nelda',
+	'Noble',
+	'Federico',
+	'Annamae',
+	'Gia',
+	'Gunner',
+	'Ines',
+	'Luther',
+	'Garrett',
+	'Travis',
+	'Chloe',
+	'Candida',
+	'Hilma',
+	'Telly',
+	'Skye',
+	'Keven',
+	'Ceasar',
+	'Selina',
+	'Emiliano',
+	'Triston',
+	'Hunter',
+	'Myrna',
+	'Adonis',
+	'Hilbert',
+	'Jamie',
+	'Trevion',
+	'Darrin',
+	'Titus',
+	'Toney',
+	'Cathryn',
+	'Astrid',
+	'Ashlee',
+	'Jacinthe',
+	'Kirstin',
+	'Petra',
+	'Tommie',
+	'Jamison'
+];
+
+export const lastNames = [
+	'Reinger',
+	'Lockman',
+	'Gleason',
+	'Wyman',
+	'Romaguera',
+	'Gerlach',
+	'Kilback',
+	'Veum',
+	'Mitchell',
+	'Reichert',
+	'Jaskolski',
+	'Price',
+	'Hill',
+	'Schaden',
+	'Lebsack',
+	'Lebsack',
+	'Gibson',
+	'Olson',
+	'Reilly',
+	'Bernier',
+	'Waelchi',
+	'Buckridge',
+	'Kuphal',
+	'Wiegand',
+	'Strosin',
+	'Zemlak',
+	'Waelchi',
+	'Russel',
+	'Gibson',
+	'Crist',
+	'Casper',
+	'Mraz',
+	'Quigley',
+	'Bailey',
+	'Stark',
+	'Wuckert',
+	'Ruecker',
+	'Carter',
+	'Franey',
+	'Howe',
+	'Daniel',
+	'Gutmann',
+	'Kutch',
+	'Leffler',
+	'Bartoletti',
+	'Balistreri',
+	'McGlynn',
+	'Lemke',
+	'Strosin',
+	'Larkin'
+];
+
+const roll6 = () => {
+	let roll6 = Math.floor(Math.random() * 6 + 1);
+	return roll6;
+};
+
+const rollGuestName = () => {
+	let roll49 = Math.floor(Math.random() * 50);
+	return roll49;
+};
+
+const rollNumGuests = () => {
+	let numOfGuests = Math.floor(Math.random() * (50 - 25 + 1) + 25);
+	return numOfGuests;
+};
+
+const getFirstName = () => {
+	let firstName = firstNames[rollGuestName()];
+	return firstName;
+};
+
+const getLastName = () => {
+	let lastName = lastNames[rollGuestName()];
+	return lastName;
+};
+
+export const generateGuests = () => {
+	let primaryKey = 1;
+	let database2 = [];
+	for (let i = 0; i < rollNumGuests(); i++) {
+		let guest = {
+			id: primaryKey,
+			firstName: getFirstName(),
+			lastName: getLastName(),
+			parkId: roll6()
+		};
+		database2.push(guest);
+		primaryKey++;
+	}
+	return database2;
+};
